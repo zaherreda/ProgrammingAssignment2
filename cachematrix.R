bc0b146ff094127ba1de9d34dc6587c9772be276
zaher.reda <zaher.reda@orkila.com>
2014-06-23 21:31:49 +0300

    Commit Comments of the Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
+  ## Set the value
   set <- function(y) {
     x <<- y
     i <<- NULL
@@ -14,12 +15,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
+  ## If in cache, returned the cached version
   if(!is.null(i)) {
     message("getting cached data")
     return(i)
   }
+  ## If not in cache, get it
   data <- x$get()
   i <- solve(data, ...)
+  
+  ## Store value in cache
   x$setinverse(i)
   i
 }
